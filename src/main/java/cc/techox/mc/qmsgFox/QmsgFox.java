d955e1d9cb2b7390e4befc4ddd352e652bedea5d
yfshi15 <yfshi15@iflytek.com>
2024-11-08 10:00:22 +0800

    初始化插件，完成装载和卸载

--- src/main/java/cc/techox/mc/qmsgFox/QmsgFox.java
+++ src/main/java/cc/techox/mc/qmsgFox/QmsgFox.java
@@ -1,5 +1,7 @@
 package cc.techox.mc.qmsgFox;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QmsgFox extends JavaPlugin {
@@ -7,11 +9,17 @@ public final class QmsgFox extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        sayConsole("§6QmsgFox plugin startup");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        sayConsole("§6QmsgFox plugin shutdown");
+    }
+
+    public void sayConsole(String consoleMsg) {
+        ConsoleCommandSender consoleSender = Bukkit.getConsoleSender();
+        consoleSender.sendMessage(consoleMsg);
     }
 }
