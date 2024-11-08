914d33e546a9c7c7bef1962650e893343ae2800d
yfshi15 <yfshi15@iflytek.com>
2024-11-08 09:37:39 +0800

    创建项目

---
+++ src/main/java/cc/techox/mc/qmsgFox/QmsgFox.java
@@ -0,0 +1,17 @@
+package cc.techox.mc.qmsgFox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QmsgFox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
